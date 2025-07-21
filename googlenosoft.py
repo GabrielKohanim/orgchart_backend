8b447ff7f07c3fcdabe1ced56b3db75887ac3262
Legal Soft <legalsoft1@LAP-SEQ-LS-SC.local>
2025-07-21 14:53:45 -0700

    first commit

--- googlenosoft.py
+++ googlenosoft.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
-from google import genai
-from google.genai import types
+from google.generativeai import genai
+from genai import types
 import asyncio
 
 
