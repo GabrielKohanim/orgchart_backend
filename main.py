d24b20ed368ab8225055ce8771f6377b3383fcc5
Legal Soft <legalsoft1@LAP-SEQ-LS-SC.local>
2025-07-22 10:12:32 -0700

    first commit

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ app = FastAPI(
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[ "https://org-chart-production.up.railway.app", "http://127.0.0.1:3001","http://localhost:3000", "http://127.0.0.1:3000", "https://org-chart-production.up.railway.app/", "https://org-chart-production.up.railway.app"],
+    allow_origins=[ "org-chart-production.up.railway.app", "org-chart-production.up.railway.app", "http://127.0.0.1:3001","http://localhost:3000", "http://127.0.0.1:3000", "https://org-chart-production.up.railway.app/", "https://org-chart-production.up.railway.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
